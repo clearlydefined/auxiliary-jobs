edec4d8b8fb38908b6fd6fded39107f0977e2eba
Gene Hazan <ghazan@microsoft.com>
2019-12-17 14:21:48 -0800

    Update index.js

--- QueuesMessageCountChecker/index.js
+++ QueuesMessageCountChecker/index.js
@@ -1,10 +1,9 @@
 // This Azure function logs Azure storage queues' message counts to Application Insights, so that alerts can be set up.
 
 const { QueueServiceClient } = require('@azure/storage-queue');
-const STORAGE_CONNECTION_STRING = process.env.STORAGE_CONNECTION_STRING;
 
 module.exports = async (context) => {
-  const queueServiceClient = QueueServiceClient.fromConnectionString(STORAGE_CONNECTION_STRING);
+  const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.STORAGE_CONNECTION_STRING);
   for await (const item of queueServiceClient.listQueues()) {
     const queueName = item.name;
     const queueClient = queueServiceClient.getQueueClient(queueName);
