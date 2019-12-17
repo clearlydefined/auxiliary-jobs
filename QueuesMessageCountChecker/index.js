751ec51e4289a1913ab4a35f34405d65f02ec3c3
Gene Hazan <ghazan@microsoft.com>
2019-12-17 14:57:11 -0800

    Attempt using applicationinsights

--- QueuesMessageCountChecker/index.js
+++ QueuesMessageCountChecker/index.js
@@ -3,11 +3,17 @@
 const { QueueServiceClient } = require('@azure/storage-queue');
 
 module.exports = async (context) => {
+  let insights = { trackEvent: context.log };
+  if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
+    const appInsights = require('applicationinsights');
+    appInsights.start();
+    insights = appInsights.defaultClient;
+  }
   const queueServiceClient = QueueServiceClient.fromConnectionString(process.env.STORAGE_CONNECTION_STRING);
   for await (const item of queueServiceClient.listQueues()) {
     const queueName = item.name;
     const queueClient = queueServiceClient.getQueueClient(queueName);
     const { approximateMessagesCount } = await queueClient.getProperties();
-    context.log({ queueName, messageCount: approximateMessagesCount } );
+    insights.trackEvent({ name: 'Queue message count', properties: { queueName, messageCount: approximateMessagesCount } });
   }
 };
