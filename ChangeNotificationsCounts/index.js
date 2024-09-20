c748f4073ddb734eee414dd6af9acf116e4d19c7
E. Lynette Rayle <elrayle@github.com>
2024-09-20 17:47:42 -0400

    move process to construct changeset name to function

--- ChangeNotificationsCounts/index.js
+++ ChangeNotificationsCounts/index.js
@@ -4,17 +4,31 @@ const insights = require('../shared/insights');
 module.exports = async (context) => {
   const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.STORAGE_CONNECTION_STRING);
   const containerName = 'changes-notifications';
-  const blobName = 'changes/2024-09-18-00';
+
+  const now = new Date();
+  const tenHoursAgo = new Date(now.getTime() - 10 * 60 * 60 * 1000);
+
+  const changesetPath = getChangesetName(tenHoursAgo);
+  context.log('changesetPath:', changesetPath);
   const containerClient = blobServiceClient.getContainerClient(containerName);
-  const blobClient = containerClient.getBlobClient(blobName);
+  const blobClient = containerClient.getBlobClient(changesetPath);
 
   const downloadBlockBlobResponse = await blobClient.download(0);
   const downloadedContent = await streamToString(downloadBlockBlobResponse.readableStreamBody);
-  const lineCount = downloadedContent.split('\n').length;
+  const allCoordinatesCount = downloadedContent.split('\n').length;
+  const goCoordinatesCount = downloadedContent.split('\n').filter(line => line.startsWith('go/golang')).length;
 
-  insights.trackEvent({ name: 'Changeset coordinate count', properties: { containerName, blobName, lineCount } });
-  context.log('Container', containerName, 'Blob', blobName, 'Line count', lineCount);
+  insights.trackEvent({ name: 'Changeset coordinate count', properties: { containerName, changesetPath, allCoordinatesCount, goCoordinatesCount } });
+  context.log('Container', containerName, 'Changeset', changesetPath, 'Coordinates Count', allCoordinatesCount, 'Go Coordinates Count', goCoordinatesCount);
 };
+
+function getChangesetName(date) {
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const hour = date.getUTCHours();
+  return `changes/${year}-${month}-${day}-${hour}`;
+}
 
 // Helper function to convert a readable stream to a string
 async function streamToString(readableStream) {
